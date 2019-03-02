a8f6e2b9cb73a217cc5901b91b4ed408d39b8c52
Andy Heil <andreas-heil@hotmail.de>
2019-03-03 00:27:04 +0100

    Refactored Bot Launcher

--- src/main/java/wtf/tks/bots/Launcher.java
+++ src/main/java/wtf/tks/bots/Launcher.java
@@ -2,18 +2,20 @@ package wtf.tks.bots;
 
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
-import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
 import org.json.simple.parser.ParseException;
 import wtf.tks.bots.Handlers.Messages;
+import wtf.tks.bots.Handlers.Reactions;
 
 import javax.security.auth.login.LoginException;
 import java.io.IOException;
 
-public class Launcher extends ListenerAdapter {
+public class Launcher {
 	
 	private static Logger log = Logger.getRootLogger();
+	
+	private static JDA jda;
 	
 	public static void main(String[] args)
 			throws LoginException, InterruptedException, IOException, ParseException {
@@ -23,11 +25,14 @@ public class Launcher extends ListenerAdapter {
 		
 		JSONObjects jsonObj = new JSONObjects("config/discordConfig.json");
 		
-		JDA jda = new JDABuilder((String)jsonObj.getObject("token"))
-				.addEventListener(new Messages())
+		jda = new JDABuilder((String) jsonObj.getObj("token"))
 				.build();
 		jda.awaitReady();
+		jda.addEventListener(new Messages(jda));
+		jda.addEventListener(new Reactions(jda));
 	}
+	
+	
 }
 
 /*
@@ -35,4 +40,9 @@ public class Launcher extends ListenerAdapter {
  	1. Flaschenpost mit RNG
  		- Command: (Direct Message) !Flaschenpost
  		- Doings:	Codec is MD
+ 		- Admin muss verifizieren
+ 			- Eigener Channel dafür mit UpDown vote System
+ 	2. Bewerbungsverfahren
+ 		- Eigener Channel
+ 		- Reaction bei ablehnung und annahme
 */
