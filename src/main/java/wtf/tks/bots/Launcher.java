a7229ce00e85a63ac1aec6a5841c8a0454619c1c
Andy Heil <andreas-heil@hotmail.de>
2019-03-11 16:17:06 +0100

    Added init Logger

--- src/main/java/wtf/tks/bots/Launcher.java
+++ src/main/java/wtf/tks/bots/Launcher.java
@@ -3,40 +3,58 @@ package wtf.tks.bots;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDABuilder;
 import net.dv8tion.jda.core.entities.Game;
-import org.apache.log4j.BasicConfigurator;
-import org.apache.log4j.Level;
-import org.apache.log4j.Logger;
+import org.apache.log4j.*;
 import org.json.simple.parser.ParseException;
+import wtf.tks.bots.Commands.Help;
 import wtf.tks.bots.Handlers.Membership;
 import wtf.tks.bots.Handlers.Messages;
 import wtf.tks.bots.Handlers.Reactions;
+import wtf.tks.bots.Handlers.SupportTracker;
 
 import javax.security.auth.login.LoginException;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class Launcher {
 	
-	private static Logger log = Logger.getRootLogger();
-	
+	private static Logger log = LogManager.getLogger(Launcher.class);
 	private static JDA jda;
+	private static Config config;
 	
 	public static void main(String[] args)
 			throws LoginException, InterruptedException, IOException, ParseException {
 		
-		BasicConfigurator.configure();
-		Logger.getRootLogger().setLevel(Level.INFO);
+		config = new Config();
+		initLogger();
 		
-		jda = new JDABuilder(Config.token)
-				.setGame(Game.listening(Config.prefix + "help"))
+		jda = new JDABuilder(config.getProp("token"))
+				.setGame(Game.listening(config.getProp("prefix") + "help"))
 				.build();
 		jda.awaitReady();
 		
 		jda.addEventListener(new Messages());
 		jda.addEventListener(new Reactions());
 		jda.addEventListener(new Membership());
+		
 	}
 	
-	public static JDA getInstance() {
+	public static JDA getJdaInstance() {
 		return jda;
+	}
+	
+	public static Config getConfigInstance() {
+		return config;
+	}
+	
+	
+	private static void initLogger() {
+		ConsoleAppender console = new ConsoleAppender();
+		String PATTERN = "%d [%p|%c|%C{1}] %m%n";
+		console.setLayout(new PatternLayout(PATTERN));
+		console.setThreshold(Level.INFO);
+		console.activateOptions();
+		log.getRootLogger().addAppender(console);
 	}
 }
